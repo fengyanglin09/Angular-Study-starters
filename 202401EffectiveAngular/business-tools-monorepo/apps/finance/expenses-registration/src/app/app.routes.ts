4f7d99f03ed89df6327e574f1ef94fc1d4145c94
lin.pengpeng <lin.pengpeng@mayo.edu>
2024-12-29 08:04:06 -0600

    adding route title to router

--- 202401EffectiveAngular/business-tools-monorepo/apps/finance/expenses-registration/src/app/app.routes.ts
+++ 202401EffectiveAngular/business-tools-monorepo/apps/finance/expenses-registration/src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Route } from '@angular/router';
+import {ResolveFn, Route, RouterStateSnapshot, TitleStrategy} from '@angular/router';
+import {ExpensesOverviewPageComponent} from "./pages/expenses-overview-page/expenses-overview-page.component";
+import {ExpensesApprovalPageComponent} from "./pages/expenses-approval-page/expenses-approval-page.component";
+import {Injectable} from "@angular/core";
+import {Title} from "@angular/platform-browser";
 
-export const appRoutes: Route[] = [];
+
+@Injectable()
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`Expenses App - ${title}`);
+    }
+  }
+}
+
+export const titleResolver: ResolveFn<string> = (route, state) =>
+  route.routeConfig?.path?.replace('-', ' ') ?? '';
+
+export const appRoutes: Route[] = [
+  { path: 'expenses-overview', component: ExpensesOverviewPageComponent, title: titleResolver },
+  { path: 'expenses-approval', component: ExpensesApprovalPageComponent, title: titleResolver },
+];
