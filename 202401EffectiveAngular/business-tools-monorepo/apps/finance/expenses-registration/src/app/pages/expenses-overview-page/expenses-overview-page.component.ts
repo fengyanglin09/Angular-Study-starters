a196e9410066b64b16e8db9207015c00fb7fe8b5
lin.pengpeng <lin.pengpeng@mayo.edu>
2025-01-07 17:37:31 -0600

    finished dynamic form

--- 202401EffectiveAngular/business-tools-monorepo/apps/finance/expenses-registration/src/app/pages/expenses-overview-page/expenses-overview-page.component.ts
+++ 202401EffectiveAngular/business-tools-monorepo/apps/finance/expenses-registration/src/app/pages/expenses-overview-page/expenses-overview-page.component.ts
@@ -1,12 +1,34 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {AddExpenseComponent, AddExpenseReactiveComponent} from "@bt-libs/finance/ui/expenses-registration-forms";
+import {
+  AddExpenseComponent,
+  AddExpenseReactiveComponent, DynamicControl,
+  DynamicFormComponent
+} from "@bt-libs/finance/ui/expenses-registration-forms";
+import {Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-expenses-overview-page',
-  imports: [CommonModule, AddExpenseComponent, AddExpenseReactiveComponent],
+  imports: [CommonModule, AddExpenseComponent, AddExpenseReactiveComponent, DynamicFormComponent],
   templateUrl: './expenses-overview-page.component.html',
   styleUrl: './expenses-overview-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ExpensesOverviewPageComponent {}
+export default class ExpensesOverviewPageComponent {
+
+  formModelConfig: DynamicControl[] = [
+    {
+      controlKey: 'description', formFieldType: 'input',
+      inputType: 'text', label: 'Description',
+      defaultValue: '', updateOn: 'change',
+      validators: [Validators.required]
+    },
+    {
+      controlKey: 'amount', formFieldType: 'input',
+      inputType: 'number', label: 'Amount excl. VAT',
+      defaultValue: null, updateOn: 'change',
+      validators: [Validators.required]
+    }
+  ]
+
+}
--- 202401EffectiveAngular/business-tools-monorepo/libs/finance/ui/expenses-registration-forms/src/index.ts
+++ 202401EffectiveAngular/business-tools-monorepo/libs/finance/ui/expenses-registration-forms/src/index.ts
@@ -4,3 +4,6 @@ export * from './lib/data-models/add-expense.interface'
 
 export * from './lib/add-expense-reactive/add-expense-reactive.component'
 
+export * from './lib/dynamic-form/dynamic-form.component'
+
+export * from './lib/data-models/dynamic-form.interface'
